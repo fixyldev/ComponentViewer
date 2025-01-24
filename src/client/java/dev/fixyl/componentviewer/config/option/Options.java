20e83f8a67a569ea11f214b5af49532e299f56c4
Fixyl <contact@fixyl.dev>
2025-01-24 18:40:00 +0100

    Fix typo

--- src/client/java/dev/fixyl/componentviewer/config/option/Options.java
+++ src/client/java/dev/fixyl/componentviewer/config/option/Options.java
@@ -26,7 +26,7 @@ package dev.fixyl.componentviewer.config.option;
 
 public interface Options {
     // Suppress the generic wildcard warning for SonarQube
-    // since it's and array of mixed types and the actual
+    // since it's an array of mixed types and the actual
     // type doesn't matter
     @SuppressWarnings("java:S1452")
     public AdvancedOption<?>[] getOptions();
