3d4d5d4d9ca30cb22b2305ce2022d277e9068fac
Fixyl <contact@fixyl.dev>
2026-03-16 13:25:42 +0100

    Fix render tick counter not incrementing at the beginning of each frame for 26.1

--- common/src/main/java/dev/fixyl/componentviewer/mixin/MinecraftMixin.java
+++ common/src/main/java/dev/fixyl/componentviewer/mixin/MinecraftMixin.java
@@ -1,7 +1,6 @@
 package dev.fixyl.componentviewer.mixin;
 
-import net.minecraft.client.DeltaTracker;
-import net.minecraft.client.renderer.GameRenderer;
+import net.minecraft.client.Minecraft;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,13 +10,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import dev.fixyl.componentviewer.ComponentViewer;
 import dev.fixyl.componentviewer.event.EventDispatcher;
 
-@Mixin(value = GameRenderer.class)
-public final class GameRendererMixin {
+@Mixin(value = Minecraft.class)
+public final class MinecraftMixin {
 
-    private GameRendererMixin() {}
+    private MinecraftMixin() {}
 
-    @Inject(method = "render(Lnet/minecraft/client/DeltaTracker;Z)V", at = @At(value = "HEAD"))
-    private void render(DeltaTracker deltaTracker, boolean renderLevel, CallbackInfo callback) {
+    @Inject(method = "renderFrame(Z)V", at = @At(value = "HEAD"))
+    private void renderFrame(boolean advanceGameTime, CallbackInfo callback) {
         ComponentViewer.dispatchEventSafely(EventDispatcher::invokeStartRenderEvent);
     }
 }
