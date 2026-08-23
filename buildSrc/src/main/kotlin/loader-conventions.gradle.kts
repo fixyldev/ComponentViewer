903a6768c390bcb68e9fe8b6f7d505bfc6549be6
Fixyl <me@fixyl.dev>
2026-08-23 14:39:18 +0200

    Refer to the common project directly instead of via paths

--- buildSrc/src/main/kotlin/loader-conventions.gradle.kts
+++ buildSrc/src/main/kotlin/loader-conventions.gradle.kts
@@ -7,6 +7,6 @@ dependencies {
 }
 
 java.sourceSets.main {
-    java.srcDir(rootDir.resolve("common/src/main/java"))
-    resources.srcDir(rootDir.resolve("common/src/main/resources"))
+    java.srcDir(project(":common").file("src/main/java"))
+    resources.srcDir(project(":common").file("src/main/resources"))
 }
